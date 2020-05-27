f0b0b9596c243bd558a49237a584b24bfc1aeb7b
Nathan McWilliams <nmcwilliams@salesforce.com>
2020-05-26 23:53:22 -0400

    configs

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    env: {
+        node: true,
+        browser: true,
+        es6: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    plugins: ['prettier'],
+    globals: {
+        GM_xmlhttpRequest: 'readonly',
+        GM_setValue: 'readonly',
+        GM_getValue: 'readonly',
+        GM_listValues: 'readonly',
+    },
+    rules: {
+        semi: 'error',
+        quotes: ['error', 'single'],
+        'no-trailing-spaces': 'warn',
+        'prettier/prettier': 'warn',
+    },
+};
